f65124b08c3b057091711098e30c973e40e1aa11
Sherry Seibel <sherry.seibel@gmail.com>
2014-10-23 17:58:43 -0400

    Add smoke test for landing page

--- spec/features/view_new_session_spec.rb
+++ spec/features/view_new_session_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 feature "User visits new session page" do
   scenario "succcessfully" do
-    visit session_path
+    visit new_session_path
 
     expect(page).to have_css "h1", text: "SHOUTER"
   end
