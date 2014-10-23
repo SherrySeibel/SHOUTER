2a0c7b1bf967aa66d5b53fcc7895b834c7937b24
Sherry Seibel <sherry.seibel@gmail.com>
2014-10-22 22:11:18 -0400

    rails new SHOUTER -T -d postgresql

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+require "monban/constraints/signed_in"
+
+Rails.application.routes.draw do
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
+
+  constraints Monban::Constraints::SignedIn.new do
+    get "/", to: "homes#show", as: :homes
+    resource :users, only: :show
+  end
+
+  root to: "sessions#new", via: :get
+end
