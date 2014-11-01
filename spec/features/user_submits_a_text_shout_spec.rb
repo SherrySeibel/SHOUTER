f0d22f0a160d41e3c6acc1eba24d948f4496f282
Sherry Seibel <sherry.seibel@gmail.com>
2014-11-01 14:38:13 -0400

    Extract tests pertaining to text_shout interaction

--- spec/features/user_submits_a_text_shout_spec.rb
+++ spec/features/user_submits_a_text_shout_spec.rb
@@ -1,43 +1,22 @@
 require "rails_helper"
 
-feature "User submits a valid text shout" do
+feature "User submits a text shout" do
   before :each do
     when_signed_in
-
-    fill_in "Shout something!", with: "HI!"
-    click_on "SHOUT!"
   end
 
-  scenario "and sees it on the homepage" do
+  scenario "with content" do
+    fill_in "Shout something!", with: "HI!"
+    click_on "SHOUT!"
+
     expect(page).to have_content "HI!"
     expect(page).to have_css ".shout, #shout_1"
   end
 
-  scenario "and clicks on time of creation" do
-    text_shout = create(:text_shout)
+  scenario "without content and sees a flash notice" do
+    fill_in "Shout something!", with: ""
+    click_on "SHOUT!"
 
-    click_on "less than a minute"
-
-    expect(page).to have_content "HI!"
-  end
-
-  scenario "and clicks on a username" do
-    text_shout = create(:text_shout)
-
-    click_on User.first.username
-
-    expect(page).to have_css "h2", text: User.first.username
-    expect(page).to have_content "HI!"
-  end
-end
-
-feature "User submmits an invalid text shout" do
-  scenario "and sees a flash notice" do
-  when_signed_in
-
-  fill_in "Shout something!", with: ""
-  click_on "SHOUT!"
-
-  expect(page).to have_content "Nothing was shouted"
+    expect(page).to have_content "Nothing was shouted"
   end
 end
