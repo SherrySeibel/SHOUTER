e2f6220acba7444ff3720e6877f4fbfe2c80928e
Sherry Seibel <sherry.seibel@gmail.com>
2014-11-04 19:18:07 -0500

    Extract User class-methods and place them into a concern

---
+++ app/models/concerns/following.rb
@@ -0,0 +1,35 @@
+module Following
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :followed_user_relationships,
+      foreign_key: :follower_id,
+      class_name: "FollowingRelationship"
+    has_many :followed_users, through: :followed_user_relationships
+
+    has_many :follower_relationships,
+      foreign_key: :followed_user_id,
+      class_name: "FollowingRelationship"
+    has_many :followers, through: :follower_relationships
+  end
+
+  def follow other_user
+    followed_users << other_user
+  end
+
+  def unfollow other_user
+    followed_users.destroy(other_user)
+  end
+
+  def following? other_user
+    followed_user_ids.include? other_user.id
+  end
+
+  def owns?(shout)
+    shout.user_id == id
+  end
+
+  def can_follow? other_user
+    self != other_user
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,37 +1,9 @@
 class User < ActiveRecord::Base
+  include Following
+
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
   validates :password_digest, presence: true
 
   has_many :shouts
-
-  has_many :text_shouts
-
-  has_many :text_shouts, through: :shouts
-
-  has_many :followed_user_relationships,
-    foreign_key: :follower_id,
-    class_name: "FollowingRelationship"
-  has_many :followed_users, through: :followed_user_relationships
-
-  has_many :follower_relationships,
-    foreign_key: :followed_user_id,
-    class_name: "FollowingRelationship"
-  has_many :followers, through: :follower_relationships
-
-  def follow other_user
-    followed_users << other_user
-  end
-
-  def unfollow other_user
-    followed_users.destroy(other_user)
-  end
-
-  def following? other_user
-    followed_user_ids.include? other_user.id
-  end
-
-  def can_follow? other_user
-    self != other_user
-  end
 end
