347704ae66a23a8a2f06dd446efce6640a5cb844
Sherry Seibel <sherry.seibel@gmail.com>
2014-11-20 16:42:27 -0500

    Change syntax around parameters

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
-  def follow_button user
-    if current_user.following? user
+  def follow_button(user)
+    if current_user.following?(user)
       button_to "Unfollow #{user.username}", user_following_relationships_path(user), method: :delete
     else
       button_to "Follow #{user.username}", user_following_relationships_path(user)
--- app/models/home.rb
+++ app/models/home.rb
@@ -1,5 +1,5 @@
 class Home
-  def initialize user
+  def initialize(user)
     @user = user
   end
 
